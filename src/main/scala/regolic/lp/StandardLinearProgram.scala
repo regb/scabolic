b535eb4df3da2897e948ada48fc4a4236eb2e80f
Regis Blanc <regis.blanc@epfl.ch>
2012-02-16 10:05:35 +0000

    Adding basic infrastructure for linear programming

---
+++ src/main/scala/regolic/lp/StandardLinearProgram.scala
@@ -0,0 +1,20 @@
+package regolic.lp
+
+import regolic.asts.core.Trees.Term
+import regolic.algebra.Rational
+import regolic.algebra.Vector
+import regolic.algebra.Matrix
+
+//a standard linear program is a canonical form of a linear program where we try to minimize 
+//a function c.x under some constraints that can be writen Ax = b with x >= 0
+//c, b and A are standard names in that situation, so we make use of them
+
+class StandardLinearProgram(val c: Vector[Rational], val A: Matrix[Rational], val b: Vector[Rational]) {
+
+  val nbConstraints = b.size
+  val nbVariables = c.size
+
+  require(A.nbRow == nbConstraints && A.nbCol == nbVariables)
+
+
+}
