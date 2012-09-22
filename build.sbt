c0ac89f5ff8a9950df6e0737fe9f4ed47a5c6ab6
Regis Blanc <regis.blanc@epfl.ch>
2012-09-22 16:07:11 +0000

    add a test-integration target to sbt

--- build.sbt
+++ build.sbt
@@ -6,5 +6,6 @@ scalaVersion := "2.9.1"
 
 scalacOptions ++= Seq("-unchecked", "-deprecation")
 
-libraryDependencies ++= 
-  Seq("org.scalatest" %% "scalatest" % "1.6.1" % "test")
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+
+testIntegration := { val exitCode = "./run-regression.sh" !; if(exitCode != 0) error("Failure") }
--- run-regression.sh
+++ run-regression.sh
@@ -6,12 +6,12 @@ echo "Running SAT solver on satisfiable instances ..."
 for f in regression/dimacs/sat/*.cnf
 do
   echo -n "Testing $f ..."
-  if ! res=`./regolic --dimacs $f 2> /dev/null`; then
+  if ! res=$(./regolic --dimacs $f 2> /dev/null); then
     echo "The solver did not exit correctly"
     error=true
   else
     echo " $res"
-    if [ $res != "sat" ]; then error=true; echo "ERROR !!!"; fi
+    if [ "$res" != "sat" ]; then error=true; echo "ERROR !!!"; fi
   fi
 done
 
@@ -19,12 +19,12 @@ echo "Running SAT solver on unsatisfiable instances ..."
 for f in regression/dimacs/unsat/*.cnf
 do
   echo -n "Testing $f ..."
-  if ! res=`./regolic --dimacs $f 2> /dev/null`; then
+  if ! res=$(./regolic --dimacs $f 2> /dev/null); then
     echo "The solver did not exit correctly"
     error=true
   else
     echo " $res"
-    if [ $res != "unsat" ]; then error=true; echo "ERROR !!!"; fi
+    if [ "$res" != "unsat" ]; then error=true; echo "ERROR !!!"; fi
   fi
 done
 
