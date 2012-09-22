046ae53ff6ec3456a94f5bdd37f270874e134220
Regis Blanc <regis.blanc@epfl.ch>
2012-09-22 23:34:40 +0000

    removed regolic script and automatically generate it with task script

--- build.sbt
+++ build.sbt
@@ -8,3 +8,16 @@ scalacOptions ++= Seq("-unchecked", "-deprecation")
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
 
+TaskKey[File]("script") <<= (baseDirectory, fullClasspath in Runtime, mainClass in Runtime) map { (base, cp, main) =>
+  val template = 
+"""#!/bin/sh
+
+java -classpath "%s" %s "$@"
+"""
+    val mainStr = main getOrElse error("No main class specified")
+    val contents = template.format(cp.files.absString, mainStr)
+    val out = base / "regolic"
+    IO.write(out, contents)
+    out.setExecutable(true)
+    out
+}
--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object Regolic extends Build {
 
   val testAll: TaskKey[Unit] = TaskKey[Unit]("test-all", "Execute unit and integration tests")
 
-  val testIntegrationTask = testIntegration := { val exitCode = "./run-regression.sh" !; if(exitCode != 0) sys.error("Failure") }
+  val testIntegrationTask = testIntegration <<= (compile in Compile) map { _ => val exitCode = "./run-regression.sh" !; if(exitCode != 0) sys.error("Failure") } 
   val testAllTask = testAll <<= (test in Test, testIntegration) map { (_, _) => ()}
 
   lazy val root = Project("root", file("."), settings = Project.defaultSettings ++ Seq(testIntegrationTask, testAllTask))
