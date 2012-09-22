aff54b6f4e14aeb55d9c839ad66c9a3af8bcd1da
Regis Blanc <regis.blanc@epfl.ch>
2012-09-22 15:19:15 +0000

    new script to run regression tests

--- run-regression.sh
+++ run-regression.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
 
+error=false
+
 echo "Running SAT solver on satisfiable instances ..."
 for f in regression/dimacs/sat/*.cnf
 do
   echo -n "Testing $f ..."
-  res=`./regolic --dimacs $f`
-  echo " $res"
-  if [ $res != "sat" ]; then echo "ERROR !!!"; fi
+  if ! res=`./regolic --dimacs $f 2> /dev/null`; then
+    echo "The solver did not exit correctly"
+    error=true
+  else
+    echo " $res"
+    if [ $res != "sat" ]; then error=true; echo "ERROR !!!"; fi
+  fi
 done
 
 echo "Running SAT solver on unsatisfiable instances ..."
 for f in regression/dimacs/unsat/*.cnf
 do
   echo -n "Testing $f ..."
-  res=`./regolic --dimacs $f`
-  echo " $res"
-  if [ $res != "unsat" ]; then echo "ERROR !!!"; fi
+  if ! res=`./regolic --dimacs $f 2> /dev/null`; then
+    echo "The solver did not exit correctly"
+    error=true
+  else
+    echo " $res"
+    if [ $res != "unsat" ]; then error=true; echo "ERROR !!!"; fi
+  fi
 done
+
+if $error; then 
+  echo "There were errors!"
+  exit 1
+fi
+
+echo "All tests successful"
+exit 0
