f605553c5fda3160e6bca8fcbfe800ce4b1ea1b8
Regis Blanc <regis.blanc@epfl.ch>
2012-11-05 08:40:27 +0100

    new class to solve linear equations over integer, in progress

---
+++ src/main/scala/regolic/equation/LinearEquationSolver.scala
@@ -0,0 +1,21 @@
+package regolic.equation
+
+/*
+ * Provides algorithms that works on a single linear equation with some variables
+ */
+
+object LinearEquationSolver {
+
+  private def coeffVar(v: Variable, t: Term): Rational = if(!contains(t, v)) Rational.zero else polynomialForm(t, v) match {
+    case Add(Mul(coeff :: Pow(v2, Num(r)) :: Nil) :: rest :: Nil) if v2==v && r.isOne => Eval(coeff, Map())
+    case Add(Mul(coeff :: Pow(v2, Num(r)) :: Nil) :: Nil) if v2==v && r.isZero => Rational.zero
+    case _ => throw new IllegalArgumentException("not a linear expression: " + t)
+  }
+
+  //find one particular instance of a solution to the equation
+  def particularSolution(eq: PredicateApplication): Map[Variable, Term] {
+    val lhSide: Term = polynomialForm(eq.map{ case Equals(t1, t2) => Sub(t1, t2) })
+    val varsToCoef: Map[Variable, Term] = 
+  }
+
+}
