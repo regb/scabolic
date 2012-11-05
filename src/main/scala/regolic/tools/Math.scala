294c0e74657251f2c9d155778f0f0ec164d476b2
Regis Blanc <regis.blanc@epfl.ch>
2012-11-05 02:40:30 +0000

    new stuff for computing gcd and extended euclid, with tests

--- src/main/scala/regolic/tools/Math.scala
+++ src/main/scala/regolic/tools/Math.scala
@@ -10,4 +10,21 @@ object Math {
 //  def comp[A,B,C](f: (A => B), g: (C => A)): (C => B) = ((x: C) => f(g(x)))
 
   def lcm(a: BigInt, b: BigInt) : BigInt = (a * b) / a.gcd(b)
+
+  def divide(a: Int, b: Int): (Int, Int) = (a / b, a % b)
+
+  def gcd(a: Int, b: Int): Int = {
+    def gcd0(a: Int, b: Int): Int = {
+      require(a >= b)
+      if(b == 0) a else gcd0(b, a % b)
+    }
+    if(a > b) gcd0(a, b) else gcd0(b, a)
+  }
+
+  //return (x, y) such that ax + by = gcd(a, b)
+  def extendedEuclid(a: Int, b: Int): (Int, Int) = if(b == 0) (1, 0) else {
+    val (q, r) = divide(a, b)
+    val (s, t) = extendedEuclid(b, r)
+    (t, s - q * t)
+  }
 }
