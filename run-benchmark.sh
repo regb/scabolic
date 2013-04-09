f865c5d85f12b608efb9c36bbe84ed67d1d038d5
Régis Blanc <regwblanc@gmail.com>
2013-04-09 22:53:42 +0200

    new benchmark script

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,6 +1,28 @@
+TIMEFORMAT=%R
+TIMEOUT=5m
+echo "#Benchmarking regolic"
+echo "Timeout is set to 5m and results are in seconds"
 for benchmarks in satlib/*; do
-  echo "=== Benchmarking $benchmarks ==="
-  if ! timeout 30m ./regolic satlib $benchmarks; then
-    echo -e "\nTotal: Timeout > 30m"
+  echo "##Benchmarking $benchmarks"
+  sum=0
+  nbsuccess=0
+  nbtimeout=0
+  for benchmark in $benchmarks/*.cnf; do
+    echo -n "$benchmark ... "
+    TIMERES=$( (time timeout $TIMEOUT ./regolic sat $benchmark > /dev/null) 2>&1)
+    if [ $? -eq 124 ]; then
+      echo "Timeout"
+      nbtimeout=$((nbtimeout + 1))
+    else
+      echo $TIMERES
+      nbsuccess=$((nbsuccess + 1))
+      sum=$(echo "$sum + $TIMERES" | bc)
+    fi
+  done
+  echo "###Number of timeouts: $nbtimeout"
+  if [ $nbsuccess -eq 0 ]; then
+    echo "###No benchmark completed"
+  else
+    echo "###Average time over successes: $(echo "$sum / $nbsuccess" | bc)"
   fi
 done
