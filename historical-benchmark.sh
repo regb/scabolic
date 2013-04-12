73ec41fd89d0fcae48b7109469da43a44167da49
Régis Blanc <regwblanc@gmail.com>
2013-04-12 23:55:05 +0200

    one file per checkout

--- historical-benchmark.sh
+++ historical-benchmark.sh
@@ -3,7 +3,8 @@ COMMITS="d947f11c2 66e5517 d43db7f3 e2294b10 94372963 391b6440"
 RESULTS_DIR="results"
 
 RUNNER=run-benchmark.sh
-SRC=src/main/scala/regolic/Main.scala src/main/scala/regolic/Settings.scala
+MAIN=src/main/scala/regolic/Main.scala
+SETTINGS=src/main/scala/regolic/Settings.scala
 
 if [ ! -d $RESULTS_DIR ]; then mkdir $RESULTS_DIR; fi
 for commit in $COMMITS; do
@@ -11,14 +12,17 @@ for commit in $COMMITS; do
   rm regolic
   git checkout $commit
   git checkout $MASTER $RUNNER
-  git checkout $MASTER $SRC
+  git checkout $MASTER $MAIN
+  git checkout $MASTER $SETTINGS
   sbt compile
   sbt script
   ./$RUNNER > $RESULTS_DIR/$commit
   git reset HEAD $RUNNER
   git checkout $RUNNER
-  git reset HEAD $SRC
-  git checkout $SRC
+  git reset HEAD $MAIN
+  git checkout $MAIN
+  git reset HEAD $SETTINGS
+  git checkout $SETTINGS
 
 done
 
