434449766071ba2173143e95b9dd96d65f486177
Régis Blanc <regwblanc@gmail.com>
2013-04-12 23:44:39 +0200

    new script across history

--- historical-benchmark.sh
+++ historical-benchmark.sh
@@ -1,22 +1,22 @@
-MASTER="120ceefd08b" #MASTER contains the needed files for running benchmarks, not necessarly master branch
-COMMITS="391b6440 dabd406b 53aebd0 8a4ff77eb1"
+MASTER="16cc4717" #MASTER contains the needed files for running benchmarks, not necessarly master branch
+COMMITS="d947f11c2 66e5517 d43db7f3 e2294b10 94372963 391b6440"
 RESULTS_DIR="results"
 
 RUNNER=run-benchmark.sh
-MAIN=src/main/scala/regolic/Main.scala
+SRC=src/main/scala/regolic/Main.scala src/main/scala/regolic/Settings.scala
 
 if [ ! -d $RESULTS_DIR ]; then mkdir $RESULTS_DIR; fi
 for commit in $COMMITS; do
 
   git checkout $commit
   git checkout $MASTER $RUNNER
-  git checkout $MASTER $MAIN
+  git checkout $MASTER $SRC
   sbt compile
   ./$RUNNER > $RESULTS_DIR/$commit
   git reset HEAD $RUNNER
   git checkout $RUNNER
-  git reset HEAD $MAIN
-  git checkout $MAIN
+  git reset HEAD $SRC
+  git checkout $SRC
 
 done
 
