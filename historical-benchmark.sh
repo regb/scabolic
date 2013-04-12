8e17c04865afab0878c8b402c124be67249e5de1
Régis Blanc <regwblanc@gmail.com>
2013-04-12 23:51:59 +0200

    fix historical benchmark

--- historical-benchmark.sh
+++ historical-benchmark.sh
@@ -8,10 +8,12 @@ SRC=src/main/scala/regolic/Main.scala src/main/scala/regolic/Settings.scala
 if [ ! -d $RESULTS_DIR ]; then mkdir $RESULTS_DIR; fi
 for commit in $COMMITS; do
 
+  rm regolic
   git checkout $commit
   git checkout $MASTER $RUNNER
   git checkout $MASTER $SRC
   sbt compile
+  sbt script
   ./$RUNNER > $RESULTS_DIR/$commit
   git reset HEAD $RUNNER
   git checkout $RUNNER
