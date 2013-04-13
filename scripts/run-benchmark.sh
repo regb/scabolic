b4f9ab8e8cd1f7ef119626249efb01425c621362
Régis Blanc <regwblanc@gmail.com>
2013-04-13 18:59:52 +0200

    improve run-benchmark

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -1,17 +1,17 @@
 TIMEFORMAT=%R
 TIMEOUT=30
-echo "#Benchmarking regolic"
+echo "#Benchmarking cafesat"
 echo "Timeout is set to ${TIMEOUT}s and results are in seconds"
-for benchmarks in satlib/*; do
+for benchmarks in $@; do
   echo "##Benchmarking $benchmarks"
   sum=0
   nbsuccess=0
   nbfailure=0
   for benchmark in $benchmarks/*.cnf; do
     echo -n "$benchmark ... "
-    TIMERES=$(./scabolic --timeout=${TIMEOUT} --time $benchmark | tail -n 1)
+    TIMERES=$(./cafesat --timeout=${TIMEOUT} --time $benchmark | tail -n 2 | head -n 1)
     status=$?
-    if [ $status -eq 124 ]; then
+    if [ $status -eq 42 ]; then
       echo "Timeout"
       nbfailure=$((nbfailure + 1))
     elif [ $status -eq 0 ]; then
@@ -19,7 +19,7 @@ for benchmarks in satlib/*; do
       nbsuccess=$((nbsuccess + 1))
       sum=$(echo "$sum + $TIMERES" | bc)
     else
-      echo "ERROR !!!"
+      echo "Error"
       nbfailure=$((nbfailure + 1))
     fi
   done
@@ -28,6 +28,7 @@ for benchmarks in satlib/*; do
     echo "###No benchmark completed"
   else
     echo "###Number of success: $nbsuccess"
+    echo "###Total time over success: $sum"
     echo "###Average time over success: $(echo "scale=3;$sum / $nbsuccess" | bc)"
     echo "###Total average time: $(echo "scale=3;($sum + ($nbfailure * $TIMEOUT)) / ($nbsuccess + $nbfailure)" | bc)"
   fi
